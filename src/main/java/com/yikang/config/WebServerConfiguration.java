6296edb4a663bcc4270bc6ab0534602e230bc268
yikang <1131487340@qq.com>
2021-04-01 17:19:54 +0800

    配置keep-alive

---
+++ src/main/java/com/yikang/config/WebServerConfiguration.java
@@ -0,0 +1,31 @@
+package com.yikang.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * 当Spring容器中没有TomcatEmbeddedServletContainerFactory这个bean时，会把此bean加载到Spring容器中
+ */
+@Component
+public class WebServerConfiguration implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+    @Override
+    public void customize(ConfigurableServletWebServerFactory factory) {
+        //使用对应工厂类提供给我们的接口定制化我们的tomcat connector
+        ((TomcatServletWebServerFactory) factory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+
+                //定制化keepalivetimeout,设置30秒内没有请求则服务端自动断开keepalive链接
+                protocol.setKeepAliveTimeout(30000);
+                //当客户端发送超过10000个请求则自动断开keepalive链接
+                protocol.setMaxKeepAliveRequests(10000);
+            }
+        });
+    }
+}

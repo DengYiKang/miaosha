ca7483c0b3afc4ee22115199c0c946a38106f3f5
yikang <1131487340@qq.com>
2021-04-15 10:38:54 +0800

    添加验证码功能

--- src/main/java/com/yikang/controller/OrderController.java
+++ src/main/java/com/yikang/controller/OrderController.java
@@ -9,6 +9,7 @@ import com.yikang.service.OrderService;
 import com.yikang.service.PromoService;
 import com.yikang.service.model.OrderModel;
 import com.yikang.service.model.UserModel;
+import com.yikang.util.CodeUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,8 +17,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.RenderedImage;
+import java.io.IOException;
 import java.sql.BatchUpdateException;
+import java.util.Map;
 import java.util.concurrent.*;
 
 @Controller("order")
@@ -49,10 +55,11 @@ public class OrderController extends BaseController {
         executorService = Executors.newFixedThreadPool(20);
     }
 
-    @RequestMapping(value = "/generatetoken", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+
+    //生成验证码
+    @RequestMapping(value = "/generateverifycode", method = {RequestMethod.POST, RequestMethod.GET})
     @ResponseBody
-    public CommonReturnType generateToken(@RequestParam(name = "itemId") Integer itemId,
-                                          @RequestParam(name = "promoId") Integer promoId) throws BusinessException {
+    public void generateVerifyCode(HttpServletResponse response) throws BusinessException, IOException {
         //根据token获取用户信息
         String token = httpServletRequest.getParameterMap().get("token")[0];
         if (StringUtils.isEmpty(token)) {
@@ -63,6 +70,36 @@ public class OrderController extends BaseController {
         if (userModel == null) {
             throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "用户尚未登录");
         }
+        Map<String, Object> map = CodeUtil.generateCodeAndPic();
+        redisTemplate.opsForValue().set("verify_code_" + userModel.getId(), map.get("code"));
+        redisTemplate.expire("verify_code_" + userModel.getId(), 10, TimeUnit.MINUTES);
+        ImageIO.write((RenderedImage) map.get("codePic"), "jpeg", response.getOutputStream());
+    }
+
+    @RequestMapping(value = "/generatetoken", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType generateToken(@RequestParam(name = "itemId") Integer itemId,
+                                          @RequestParam(name = "promoId") Integer promoId,
+                                          @RequestParam(name = "verifyCode") String verifyCode) throws BusinessException {
+        //根据token获取用户信息
+        String token = httpServletRequest.getParameterMap().get("token")[0];
+        if (StringUtils.isEmpty(token)) {
+            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "用户尚未登录");
+        }
+        //获取用户登录信息
+        UserModel userModel = (UserModel) redisTemplate.opsForValue().get(token);
+        if (userModel == null) {
+            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "用户尚未登录");
+        }
+        //通过verifycode验证验证码的有效性
+        String verifyCodeInRedis = (String) redisTemplate.opsForValue().get("verify_code_" + userModel.getId());
+        if (StringUtils.isEmpty(verifyCodeInRedis)) {
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "请求非法");
+        }
+        if (!verifyCodeInRedis.equalsIgnoreCase(verifyCode)) {
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "验证码错误");
+        }
+
         //获取秒杀访问令牌
         String promoToken = promoService.generateSecondKillToken(promoId, itemId, userModel.getId());
         if (promoToken == null) {
@@ -88,6 +125,7 @@ public class OrderController extends BaseController {
         if (userModel == null) {
             throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "用户尚未登录");
         }
+
         String key = "promo_token_" + promoId + "_userid_" + userModel.getId() + "_itemid" + itemId;
         //校验秒杀令牌是否正确
         if (promoToken != null) {
